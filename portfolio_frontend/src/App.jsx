eb44bd43e9f6e1f0d8946d9cd1136088c72c2a18
spencergamero23 <bluerayman23@gmail.com>
2025-05-14 15:35:41 -0700

    Added some comments and made some changes

--- portfolio_frontend/src/App.jsx
+++ portfolio_frontend/src/App.jsx
@@ -9,8 +9,11 @@ function App() {
 
   return (
     <>
+    {/*Here is where the tags that actually make up the site are. Below is the navbar  */}
       <NavBar/>
+      {/* Where the main content is stored */}
       <main className='main-content'>
+        {/* Heres how the links actually work for react. */}
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/contact' element={<Contact/>}/>
